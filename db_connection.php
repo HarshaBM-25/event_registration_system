5fb5386be8f58c9d96c3c032f2c6462f2985b902
HarshaBM-25 <harshabm16@gmail.com>
2025-05-10 20:22:03 +0530

    Add files via upload
    my first project

---
+++ db_connection.php
@@ -0,0 +1,13 @@
+.
+<?php
+$servername = "localhost"; // Change to your database host if different
+$username = "root";        // Your database username
+$password = "";            // Your database password
+$dbname = "event_db";      // Your database name
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
